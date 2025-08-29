766a753be06334e84adfe17b867453c95986a8a5
Muhammad Surya Saniansyah <muhammadsurya216@gmail.com>
2025-08-29 13:28:25 +0800

    feat: improve dialog development plan

--- src/components/development/DevelopmentPage.jsx
+++ src/components/development/DevelopmentPage.jsx
@@ -20,16 +20,21 @@ import {
 } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 
-export default function DevelopmentPage({ developments, params }) {
+export default function DevelopmentPage({ developments }) {
 	const { isOpen } = useSidebar();
 
-	const { developmentId } = React.use(params);
+	// Dialog Detail
+	const [openDialog, setOpenDialog] = React.useState(false);
+	const [dialogTitle, setDialogTitle] = React.useState(
+		"Add Development Plan - Training"
+	);
+	const [selectedRow, setSelectedRow] = React.useState(null);
 
 	const columns = [
 		{
 			key: "id",
 			header: "No.",
-			className: "text-center font-semibold w-[100px]",
+			className: "text-center font-semibold md:w-[100px]",
 			cellClassName: "text-center font-semibold",
 		},
 		{
@@ -51,12 +56,16 @@ export default function DevelopmentPage({ developments, params }) {
 			className: "text-center",
 			render: (_, row) => (
 				<ActionButton
-					as="link"
-					href={"#"}
+					as="button"
 					icon={<Eye className="size-5 stroke-white" />}
 					tooltip="Detail"
 					color="bg-info"
 					arrowFill="fill-info"
+					onClick={() => {
+						setSelectedRow(row);
+						setDialogTitle(`${row.year} Development Plan`);
+						setOpenDialog(true);
+					}}
 				/>
 			),
 		},
@@ -70,122 +79,27 @@ export default function DevelopmentPage({ developments, params }) {
 			)}
 		>
 			<div className="w-full flex justify-between items-center mb-5 md:mb-10 flex-col md:flex-row gap-10 md:gap-5">
-				<h1 className="text-primary text-4xl font-bold">
-					My Development {developmentId}
-				</h1>
+				<h1 className="text-primary text-4xl font-bold">My Development</h1>
 
 				<div className="flex gap-2 w-full md:w-[350px]">
-					<Dialog>
-						<DialogTrigger asChild>
-							<Button
-								variant={"primary"}
-								className={"ms-auto h-[2.5rem] w-[5.8rem]"}
-								size={"xl"}
-							>
-								<Plus strokeWidth={3} /> Add
-							</Button>
-						</DialogTrigger>
-
-						<DialogContent
-							className={
-								"w-[90vw] sm:max-w-none md:w-[50vw] max-h-[80vh] overflow-y-auto flex flex-col px-8 pt-6"
-							}
-						>
-							<form>
-								<DialogHeader>
-									<DialogTitle className={"text-xl md:text-3xl font-bold"}>
-										Add Development Plan - Training
-									</DialogTitle>
-									<div className="w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
-
-									<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
-										<SelectInput
-											id={"group-competency"}
-											className={"grid gap-3 col-span-1"}
-											labelTitle={"Group Competency"}
-											content={["BC", "BMC", "MDP"]}
-										/>
-										<SelectInput
-											id={"group-competency"}
-											className={"grid gap-3 col-span-1"}
-											labelTitle={"Competency"}
-											content={[
-												"Communication",
-												"Problem Solving",
-												"Project Manager",
-											]}
-										/>
-									</div>
-									<div className="block md:hidden w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
-
-									<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
-										<SelectInput
-											id={"group-competency"}
-											className={"grid gap-3 col-span-1"}
-											labelTitle={"Group Competency"}
-											content={["BC", "BMC", "MDP"]}
-										/>
-										<SelectInput
-											id={"group-competency"}
-											className={"grid gap-3 col-span-1"}
-											labelTitle={"Competency"}
-											content={[
-												"Communication",
-												"Problem Solving",
-												"Project Manager",
-											]}
-										/>
-									</div>
-									<div className="block md:hidden w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
-
-									<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
-										<SelectInput
-											id={"group-competency"}
-											className={"grid gap-3 col-span-1"}
-											labelTitle={"Group Competency"}
-											content={["BC", "BMC", "MDP"]}
-										/>
-										<SelectInput
-											id={"group-competency"}
-											className={"grid gap-3 col-span-1"}
-											labelTitle={"Competency"}
-											content={[
-												"Communication",
-												"Problem Solving",
-												"Project Manager",
-											]}
-										/>
-									</div>
-									<div className="block md:hidden w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
-
-									<DialogFooter className={"flex flex-row justify-end mt-4"}>
-										<DialogClose asChild>
-											<Button
-												className={
-													"bg-accent text-accent-foreground hover:opacity-70"
-												}
-												size={"xl"}
-											>
-												Close
-											</Button>
-										</DialogClose>
-										<Button
-											variant={"primary"}
-											className={"ms-auto"}
-											size={"xl"}
-										>
-											Save <Save strokeWidth={2} />
-										</Button>
-									</DialogFooter>
-								</DialogHeader>
-							</form>
-						</DialogContent>
-					</Dialog>
+					<Button
+						variant={"primary"}
+						className={"ms-auto h-[2.5rem] w-[5.8rem]"}
+						size={"xl"}
+						onClick={() => {
+							setSelectedRow(null);
+							setDialogTitle("Add Development Plan - Training");
+							setOpenDialog(true);
+						}}
+					>
+						<Plus strokeWidth={3} /> Add
+					</Button>
 
 					<SearchInput className={"w-[15rem] md:w-full"} />
 				</div>
 			</div>
 
+			{/* Table */}
 			<div className="rounded-lg border border-gray-200 shadow-all p-2">
 				<DataTable columns={columns} rows={developments} />
 			</div>
@@ -197,6 +111,100 @@ export default function DevelopmentPage({ developments, params }) {
 
 				<MyPagination />
 			</div>
+
+			{/* Dialog Add */}
+			<Dialog open={openDialog} onOpenChange={setOpenDialog}>
+				<DialogContent
+					className={
+						"w-[90vw] sm:max-w-none md:w-[50vw] max-h-[80vh] overflow-y-auto flex flex-col px-8 pt-6"
+					}
+				>
+					<form>
+						<DialogHeader>
+							<DialogTitle className={"text-xl md:text-3xl font-bold"}>
+								{dialogTitle}
+							</DialogTitle>
+							<div className="w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
+
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
+								<SelectInput
+									id={"group-competency"}
+									className={"grid gap-3 col-span-1"}
+									labelTitle={"Group Competency"}
+									content={["BC", "BMC", "MDP"]}
+								/>
+								<SelectInput
+									id={"group-competency"}
+									className={"grid gap-3 col-span-1"}
+									labelTitle={"Competency"}
+									content={[
+										"Communication",
+										"Problem Solving",
+										"Project Manager",
+									]}
+								/>
+							</div>
+							<div className="block md:hidden w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
+
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
+								<SelectInput
+									id={"group-competency"}
+									className={"grid gap-3 col-span-1"}
+									labelTitle={"Group Competency"}
+									content={["BC", "BMC", "MDP"]}
+								/>
+								<SelectInput
+									id={"group-competency"}
+									className={"grid gap-3 col-span-1"}
+									labelTitle={"Competency"}
+									content={[
+										"Communication",
+										"Problem Solving",
+										"Project Manager",
+									]}
+								/>
+							</div>
+							<div className="block md:hidden w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
+
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
+								<SelectInput
+									id={"group-competency"}
+									className={"grid gap-3 col-span-1"}
+									labelTitle={"Group Competency"}
+									content={["BC", "BMC", "MDP"]}
+								/>
+								<SelectInput
+									id={"group-competency"}
+									className={"grid gap-3 col-span-1"}
+									labelTitle={"Competency"}
+									content={[
+										"Communication",
+										"Problem Solving",
+										"Project Manager",
+									]}
+								/>
+							</div>
+							<div className="block md:hidden w-full h-[2px] bg-gray-200 mt-2 mb-2 md:mb-3 md:mt-3" />
+
+							<DialogFooter className={"flex flex-row justify-end mt-4"}>
+								<DialogClose asChild>
+									<Button
+										className={
+											"bg-accent text-accent-foreground hover:opacity-70"
+										}
+										size={"xl"}
+									>
+										Close
+									</Button>
+								</DialogClose>
+								<Button variant={"primary"} className={"ms-auto"} size={"xl"}>
+									Save <Save strokeWidth={2} />
+								</Button>
+							</DialogFooter>
+						</DialogHeader>
+					</form>
+				</DialogContent>
+			</Dialog>
 		</div>
 	);
 }
